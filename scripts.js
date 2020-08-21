706b5c0d881e23aae60a23f81046a446130ab0d5
esgowens <kinggsone@gmail.com>
2020-08-21 15:02:58 -0400

    some fixes to the nav

--- scripts.js
+++ scripts.js
@@ -3,7 +3,20 @@ barba.use(barbaCss)
 barba.init({
     transitions: [
         {
-            name: 'fade'
+            name: 'fade',
+            beforeEnter ({ current, next, trigger }) {
+                
+                const headerLinks = document.querySelectorAll("header a")
+
+                headerLinks.forEach(link => {
+                    link.classList.remove("selected")
+                })
+
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                })
+            }
         }
     ]
 })
